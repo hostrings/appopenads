9e50ae554c0569e51b99c593e0d400ba2375efe7
chandan <26101741+notchandan@users.noreply.github.com>
2020-10-13 15:35:52 +0530

    create and register Application subclass

---
+++ demo/src/main/java/app/chandan/demo/MyApp.java
@@ -0,0 +1,10 @@
+package app.chandan.demo;
+
+import android.app.Application;
+
+public class MyApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
