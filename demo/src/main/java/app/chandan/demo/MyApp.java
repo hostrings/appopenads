51d023d6e97cdafe87d1c7c9cf842d0090242448
chandan <26101741+notchandan@users.noreply.github.com>
2020-10-13 15:41:36 +0530

    initialize app open ads

--- demo/src/main/java/app/chandan/demo/MyApp.java
+++ demo/src/main/java/app/chandan/demo/MyApp.java
@@ -4,13 +4,17 @@ import android.app.Application;
 
 import com.google.android.gms.ads.MobileAds;
 
+import app.chandan.appopenads.AppOpenManager;
+
 public class MyApp extends Application {
     public static final boolean are_you_testing = true;
+    private AppOpenManager<MyApp> appOpenManager;
 
     @Override
     public void onCreate() {
         super.onCreate();
         MobileAds.initialize(this);
         MoneyIds.AreYouTesting();
+        appOpenManager = new AppOpenManager<>(this, MoneyIds.getAppOpenAdId());
     }
 }
